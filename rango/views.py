ab14150145e70ea044af8ebba1539596d4ceaa8d
Logan Brown <LoganBrownGU>
2023-01-23 10:00:50 +0000

    added about view and index.html

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there")
+    return HttpResponse("Rango says hey there <a href='/rango/about'>ABOUT</a>")
+
+def about(request):
+    return HttpResponse("Rango says ABOUT <a href='/rango/'>HOME</a>")
+
