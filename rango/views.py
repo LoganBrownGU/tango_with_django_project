1c1b5c36753a505f62a71acf8b13c3a85ad3d7b4
Logan Brown <LoganBrownGU>
2023-01-23 15:24:29 +0000

    added template for about, MEDIA TAG NOT WORKING

--- rango/views.py
+++ rango/views.py
@@ -11,5 +11,8 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says ABOUT <a href='/rango/'>HOME</a>")
+    context_dict = {'boldmessage': 'Here\'s another teapot'}
 
+    return render(request, 'rango/about.html', context=context_dict)
+
+    #return HttpResponse("Rango says ABOUT <a href='/rango/'>HOME</a>")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.conf import settings
-from django.conf.urls.static import stati
+from django.conf.urls.static import static
 
 from rango import views
 
