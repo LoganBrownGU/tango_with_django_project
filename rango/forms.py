58041614637aae2c856a47f37b05dd1cf99b0b20
Logan Brown <LoganBrownGU>
2023-02-01 10:41:04 +0000

    add cat form working

--- rango/forms.py
+++ rango/forms.py
@@ -4,8 +4,8 @@ from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Enter category name")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0, required=False)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0, required=False)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
