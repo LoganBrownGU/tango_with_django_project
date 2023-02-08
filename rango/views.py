d9778c108d7d61c7cbe0515ff560a34ec1ce855c
Logan Brown <LoganBrownGU>
2023-02-08 15:18:56 +0000

    added login functionality

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
@@ -114,3 +115,23 @@ def register(request):
         "profile_form": profile_form,
         "registered": registered,
     })
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active():
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your rango account has been disabled nerd")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied")
+    else:
+        return render(request, 'rango/login.html')
+        
